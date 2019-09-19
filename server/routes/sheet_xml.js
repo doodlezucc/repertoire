ff6dac5b13f65addb4e72a1d7b884ad3f60d4b2d
doodlezucc <electerminator@gmail.com>
2019-09-19 17:28:01 +0200

    prepared server stuff

--- server/routes/sheet_xml.js
+++ server/routes/sheet_xml.js
@@ -1,60 +1,133 @@
 var express = require("express");
 var jimp = require("jimp");
 var path = require("path");
+var spawn = require("child_process").spawn;
 var exec = require("child_process").exec;
+var jo = require("jpeg-autorotate");
+var fs = require("fs");
 var router = express.Router();
 
-router.post("/", function(req, res){
+const audiverisPath = "/home/tappi/audiveris/";
+const directoryPath = "/home/tappi/lal/";
+
+router.post("/", function (req, res) {
   print("lul");
   var name = req.body.name;
   var img = req.body.image;
-  var realFile = Buffer.from(img,"base64");
-  fs.writeFile(name, realFile, function(err) {
-      if(err)
-         console.log(err);
-   });
-   res.send("OK");
- });
+  var realFile = Buffer.from(img, "base64");
+  fs.writeFile(name, realFile, function (err) {
+    if (err)
+      console.log(err);
+  });
+  res.send("OK");
+});
 
 module.exports = router;
 
 
 
 
-/**
- * Executes a shell command and return it as a Promise.
- * @param cmd {string}
- * @return {Promise<string>}
- */
-function execShellCommand(cmd) {
-  const exec = require('child_process').exec;
+function spawnPretty(cmd, args) {
   return new Promise((resolve, reject) => {
-   exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-     console.warn(error);
-    }
-    resolve(stdout? stdout : stderr);
-   });
-  });
- }
+    console.log(cmd);
+    var child = spawn(cmd, args);
 
-var input = path.join(__dirname, "sheet.JPG");
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function (data) {
+      //Here is where the output goes
+
+      console.log('stdout: ' + data);
+
+      data = data.toString();
+    });
+
+    child.on("close", function () {
+      resolve();
+    });
+  });
+}
+
+
+var input = path.join(__dirname, "try2.JPG");
+//drainExif(input);
 //interpretImageAsMusicXML(input);
+processTask(input);
 
-async function interpretImageAsMusicXML(imgFile, cb) {
-  console.log("removing orientation");
-  await execShellCommand("exiftool -Orientation=1 " + imgFile);
-  console.log("pre-processing image");
+async function processTask(file) {
+  const low = file.toLowerCase();
+  if (low.endsWith(".png") || low.endsWith(".jpg") || low.endsWith(".gif")) {
+    console.log("Dealing with an image");
+    file = await drainExif(file);
+  }
+  fileToMusicXml(file);
+  //console.log(std);
+}
 
-  jimp.read(imgFile).then(img => {
-    img
-      .greyscale()
-      .brightness(0.25)
-      .contrast(1)
-      .write(path.join(__dirname, "out.jpg"));
-    console.log("yayeya");
-  })
-  .catch(err => {
-    console.error(err);
+function fileToMusicXml(file) {
+  console.log("magically turning file into musicxml");
+  spawnPretty(audiverisPath + "gradlew", ["run", "--project-dir", audiverisPath, "-PcmdLineArgs", "\"-batch,-export,-output," + directoryPath + ",--," + file + "\""]);
+}
+
+
+function drainExif(file) {
+  return new Promise((resolve, reject) => {
+    jo.rotate(file, { quality: 85 })
+      .then(({ buffer, orientation, dimensions, quality }) => {
+        console.log(`Orientation was ${orientation}`)
+        console.log(`Dimensions after rotation: ${dimensions.width}x${dimensions.height}`)
+        console.log(`Quality: ${quality}`);
+        var output = path.join(__dirname, "normalized.jpg");
+        fs.writeFile(output, buffer, function (err) {
+          console.log(err ? err : "yawww");
+          resolve(output);
+        });
+      })
+      .catch((error) => {
+        if (error.code === jo.errors.correct_orientation) {
+          resolve(file);
+        } else {
+          console.log('An error occurred when rotating the file: ' + error.message);
+          reject(error);
+        }
+      });
   });
+}
+
+function preprocessImage(file) {
+  console.log("pre-processing image");
+  return new Promise(resolve => {
+    jimp.read(file)
+      .then(img => {
+        var start = Date.now();
+        img
+          .scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, idx) => {
+            var v = img.bitmap.data[idx + 1];
+            if (v > 110) {
+              v = 255;
+            } else {
+              v = 0;
+            }
+            img.bitmap.data[idx + 0] = v;
+            img.bitmap.data[idx + 1] = v;
+            img.bitmap.data[idx + 2] = v;
+          });
+        var diff = Date.now() - start;
+        console.log("Millis passed: " + diff);
+        resolve(img);
+        img.write(path.join(__dirname, "out.jpg"));
+      });
+  });
+}
+
+function straightenSheet(img) {
+  return new Promise(resolve => {
+
+  });
+}
+
+async function interpretImageAsMusicXML(file, cb) {
+  //drainExif();
+  var prep = await preprocessImage(file);
+
+  console.log("yayeya");
 }
